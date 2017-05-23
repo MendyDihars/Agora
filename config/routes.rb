dd78af97c6196d0be38b9555c7c64dae70bd2050
Gilles Aroutzet <aroutzet@gmail.com>
2017-05-23 16:53:55 +0200

    meeting routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :teachers, only: [:index, :show]
+  resources :teachers, only: [:index, :show] do
+    resources :meetings, only: [:create]
+  end
+  resources :meetings, only: [:index]
   resources :students, only: [:index]
 end
