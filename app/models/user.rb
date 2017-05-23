9cbd1f9e8b8a2bafb394505fcffef54fa78059b5
Mendy Dihars <mendy.dihars@laposte.net>
2017-05-23 14:53:00 +0200

    Remove validates :age

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,5 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :age, presence: true
   validates :address, presence: true
 end
