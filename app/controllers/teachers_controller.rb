f1e182769adbc4f7da43c6ee3fea327345e0b5c1
Denis giraud <giraud.de@gmail.com>
2017-05-23 17:58:54 +0200

    index search mechanic and show achieved

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,11 +2,27 @@ class TeachersController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
+    @teachers = User.all
 
+    if search_params[:user_skill].present?
+      @teachers = User.joins(:user_skills).where(user_skills:
+        { skill_id: Skill.find_by("name =  ?", search_params[:user_skill].capitalize) }
+      )
+    end
+
+    if search_params[:address].present?
+      @teachers = @teachers.near(search_params[:address])
+    end
+
+    @teachers = @teachers.where.not(latitude: nil, longitude: nil)
   end
 
   def show
+    @teachers = User.find(params[:id])
+  end
 
+  def search_params
+    params.require(:index).permit(:user_skill, :address)
   end
 
 end
