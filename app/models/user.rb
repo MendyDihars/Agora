eedf9e503b3ee6abcc3bc63d42401dbb2e872f59
Mendy Dihars <mendy.dihars@laposte.net>
2017-05-23 13:42:31 +0200

    validates user added

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
   has_many :meetings, class_name: 'User', foreign_key: "student_id"
   has_many :own_skills, through: :user_skills, source: :skill
   has_many :wanted_skills, through: :requested_skills, source: :skill
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+  validates :address, presence: true
 end
