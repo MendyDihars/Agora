36308cedc12c40ba765bec7d2382a50260590ee2
Gilles Aroutzet <aroutzet@gmail.com>
2017-05-24 16:46:32 +0200

    Review controller and form teacher show ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,21 @@
 class ReviewsController < ApplicationController
 
   def create
-<<<<<<< HEAD
-    @teacher = User.find(teacher_id)
+    @teacher = User.find(params[:teacher_id])
     @review = Review.new(review_params)
     @review.teacher = @teacher
     @review.student = current_user
     if @review.save
       redirect_to teacher_path(@teacher)
     else
-      render
-    end "teacher/show"
+      render "teacher/show"
+    end
   end
 
   private
 
   def review_params
     params.require(:review).permit(:content, :rating)
-  end
-=======
->>>>>>> add-reviews
-
   end
 
 end
