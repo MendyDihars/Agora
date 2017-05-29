1d3f86e833c22a712d3ed0cd64c82c79ae6f41f9
Denis giraud <giraud.de@gmail.com>
2017-05-29 16:37:57 +0200

    validates done and some refacto

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ApplicationRecord
   belongs_to :student, class_name: "User", foreign_key: "student_id"
   belongs_to :meeting
 
-  validates :content, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }, numericality: { only_integer: true }
+  validates :content, length: { in: 15..300 }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false },
+              numericality: { only_integer: true }
 end
