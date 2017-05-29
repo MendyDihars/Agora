02e45d729d8e8d1be452105b523838a8f6d0707c
Denis giraud <giraud.de@gmail.com>
2017-05-29 12:41:56 +0200

    added validation to rating review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   belongs_to :meeting
 
   validates :content, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }, numericality: { only_integer: true }
 end
