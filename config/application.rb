6cec23b9266ea310d8c41bd8bab37b283bb8f1d3
Mendy Dihars <mendy.dihars@laposte.net>
2017-05-31 15:45:40 +0200

    Assets true does'nt work

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,6 @@ module Agora
       generate.helper false
     end
 
-    config.serve_static_assets = true
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
