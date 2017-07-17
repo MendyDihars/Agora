3cebb5211de2063b299d9f615b24513440952dda
Mendy Dihars <mendy.dihars@laposte.net>
2017-07-17 15:38:46 +0200

    Modify search form and controller for select address

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   layout 'home'
+
   def home
+    pre_addresses = User.all.map do |user|
+      user.address
+    end
+    @addresses = pre_addresses.uniq
   end
 end
